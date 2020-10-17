873d56259dd306290009fcc7248dae4971d3192b
Bartosz Maziarka <bahtek.maziarka@gmail.com>
2020-10-17 13:46:30 +0200

    Moved type dicts outside the loop

--- spendings/main.py
+++ spendings/main.py
@@ -1,6 +1,30 @@
 import csv
 
 umowy_dict = {}
+switch_row_12 = {
+    "CODZIENNE_ZAKUPY": 0,
+    "ZDROWIE": 1,
+    "PODRÓ¯E": 2,
+    "ZAKUPY_INT": 2,
+    "SAMOCHÓD": 3,
+    "UBRANIA": 4,
+    "DLA_DOMU": 5,
+    "BIUROWE": 5,
+    "CZAS_WOLNY": 6,
+    "DZIECI": 7,
+    "RACHUNKI": 9,
+    "RACHUNKI_DOMOWE": 9,
+    "WYPLATA_BANKOMAT": 10,
+}
+switch_row_11 = {
+    "WCZASY": 2,
+    "PODRÓŻE": 2,
+    "FAST_FOOD": 6,
+    "RESTAURACJA": 6,
+    "CHARYTATYWNE": 8,
+    "WYP£ATA_GOTÓWKOWA": 10,
+    "PAYU_PAYPAL": 11,
+}
 with open('UMOWY_STANY.csv', newline='') as umowy:
     reader_umowy = csv.reader(umowy, delimiter=',', quotechar='|')
     for row in reader_umowy:
@@ -12,30 +36,6 @@ with open('input.csv', newline='', encoding="utf8") as csvfile:
         for row in reader:
             if float(row[4]) > 0 and (row[11] != '' or row[12] != ''):
                 case_type = -1
-                switch_row_12 = {
-                    "CODZIENNE_ZAKUPY": 0,
-                    "ZDROWIE": 1,
-                    "PODRÓ¯E": 2,
-                    "ZAKUPY_INT": 2,
-                    "SAMOCHÓD": 3,
-                    "UBRANIA": 4,
-                    "DLA_DOMU": 5,
-                    "BIUROWE": 5,
-                    "CZAS_WOLNY": 6,
-                    "DZIECI": 7,
-                    "RACHUNKI": 9,
-                    "RACHUNKI_DOMOWE": 9,
-                    "WYPLATA_BANKOMAT": 10,
-                }
-                switch_row_11 = {
-                    "WCZASY": 2,
-                    "PODRÓŻE": 2,
-                    "FAST_FOOD": 6,
-                    "RESTAURACJA": 6,
-                    "CHARYTATYWNE": 8,
-                    "WYP£ATA_GOTÓWKOWA": 10,
-                    "PAYU_PAYPAL": 11,
-                }
                 case_type = switch_row_12.get(row[12], -1)
                 if case_type == -1:
                     case_type = switch_row_11.get(row[11], -1)
